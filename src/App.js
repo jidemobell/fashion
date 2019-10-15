d9984301ed567c403f17396c795dc2aede8cbb29
jidemobell <jidd32@gmail.com>
2019-10-15 09:11:10 +0100

    make app responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import prinmia from './image/prinmia2.jpeg';
-import signature from './image/signature.png'
-import brand from './image/brand.PNG'
-import './App.css';
+import React from "react";
+import prinmia from "./image/prinmia2.jpeg";
+import signature from "./image/signature.png";
+import brand from "./image/brand.PNG";
+import './responsive.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App flex-row">	  	
-		  <section className="image">
-			 <img src={prinmia} alt='background' />
-			</section>	
-			<section className="content">
-			  <header className="flex-row header">
-					<img src={brand} alt="signature" className="brand" />
-					<img src={signature} alt="signature" className="signature" />
-				</header>
-				<div className="flex-col-centered body">
-					<p id='first' className="josefina">THIS WEBSITE IS UNDER CONSTRUCTION</p>
-					<p id='second' className='robo-mono'>Coming Soon.</p>
-				</div>
-			</section>
+    <div className="App flex-row">
+      <section className="content">
+        <header className="flex-row header">
+          <img src={brand} alt="signature" className="brand" />
+          <img src={signature} alt="signature" className="signature" />
+        </header>
+        <div className="flex-col-centered body">
+          <p id="first" className="josefina">
+            THIS WEBSITE IS UNDER CONSTRUCTION
+          </p>
+          <p id="second" className="robo-mono">
+            Coming Soon.
+          </p>
+        </div>
+      </section>
+      <section className="image">
+        <img src={prinmia} alt="background" />
+      </section>
     </div>
   );
 }
