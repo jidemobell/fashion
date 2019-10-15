99bb677f517146676955b6d5d9e14668bd6d9678
jidemobell <jidd32@gmail.com>
2019-10-15 08:13:42 +0100

    Updates

---
+++ precache-manifest.c316a373845e883b1e44327348415870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b13f5d751f9c9383fc986c977c6bb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b8927d962545f9ae662",
+    "url": "/static/css/main.2c14b5ce.chunk.css"
+  },
+  {
+    "revision": "fb0dea4a8fb8e9856624",
+    "url": "/static/js/2.ba1eafc2.chunk.js"
+  },
+  {
+    "revision": "4b8927d962545f9ae662",
+    "url": "/static/js/main.fc50ab75.chunk.js"
+  },
+  {
+    "revision": "d549e1ade04d4922fac1",
+    "url": "/static/js/runtime-main.bd96db3e.js"
+  },
+  {
+    "revision": "9e9e2ee862e5de03fcfe4a7eca08da73",
+    "url": "/static/media/brand.9e9e2ee8.PNG"
+  },
+  {
+    "revision": "3ff5b4fd587baf8fad44a3c4043ae7a4",
+    "url": "/static/media/prinmia2.3ff5b4fd.jpeg"
+  }
+]);
